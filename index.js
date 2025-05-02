9c1ac94955e5138cf8464f4ac20b0c60392939ad
Toyosi-james <Jamestoyosi84@gmail.com>
2025-05-02 10:00:52 +0100

    My javascript challenge

---
+++ index.js
@@ -0,0 +1,33 @@
+// comparison operator
+const d ="jane";
+const e ="jane"
+
+console.log(d==e)
+
+// greater than and lesser than
+
+const x = 77
+const y = 99
+
+console .log(x > y)
+console.log(x<y)
+
+// greater than or equals to and lesser than or equals 
+const m = 30
+const n = 20
+console.log(m >= n)
+console.log(m <= n)
+
+// logical operator
+//   OR
+let isSunny = false
+let isRainny= false
+let answer=isSunny||isRainny
+console.log(answer)
+
+const male = false
+const female = true
+const gender =male || female && isSunny || isRainny
+console.log(gender)
+
+// Ternary operator
