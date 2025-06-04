237546d92d69d83091df5e3d242bfdb3b7318805
Toyosi-james <Jamestoyosi84@gmail.com>
2025-06-04 22:17:25 +0100

    Javascript challenge

--- Javascript-challeng2/script.js
+++ Javascript-challeng2/script.js
@@ -6,3 +6,27 @@ setTimeout(function (){
 console.log('End')
 
 fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => console.log(data))
+
+async function getJoke(){
+    try{
+        let reply = await fetch('https://official-joke-api.appspot.com/random_joke');
+        let joke = await reply.json()
+        document.getElementById('joke-setup').innerText= joke.setup
+        document.getElementById('joke-punchline').innerText = joke.punchline
+    }catch(error){
+        console.log('oops something went wrong')
+    }
+}
+
+const getAdvice =async ()=>{
+    try{
+        let response= await fetch('https://api.adviceslip.com/advice')
+        let answer =await response.json()
+
+        document.getElementById("Advice-setup").innerText = answer.slip.advice
+
+        document.getElementById("Advice-Response").innerText= answer.slip.id
+    }catch(error){
+        console.log('oops! Advice gone wrong')
+    }
+}
